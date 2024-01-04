68f9b8c957a9710cfa1b1b518a6d226f8e0a1bc1
savioarthur00 <savio1281@gmail.com>
2024-01-04 10:29:09 -0300

    validador de email ao registrar novo usuario

--- FisioAppGraph/frontend/src/componentes/usuario/Novo.vue
+++ FisioAppGraph/frontend/src/componentes/usuario/Novo.vue
@@ -1,143 +1,163 @@
 <template>
     <v-container fluid>
-        <v-layout>
-            <v-flex>
-                <v-layout column class="ma-3">
-                    <h1 class="headline">Novo Usuário</h1>
-                    <v-divider class="mb-3" />
-                        <div v-if="erros">
-                            <Erros :erros="erros" />
-                        </div>
-                        <v-text-field label="Nome"
-                            v-model="usuario.nome" />
-                        <v-text-field label="E-mail"
-                            v-model="usuario.email" />
-                        <v-text-field label="Senha"
-                            v-model="usuario.senha" type="password" />
-                        <v-select label="Perfis"
-                            v-model="usuario.perfis"
-                            :items="perfis"
-                            item-value="id"
-                            item-text="rotulo"
-                            attach multiple
-                            chips deletable-chips />
-                        <v-btn class="ml-0 mt-3"
-                            @click="obterPerfis">
-                            Obter Perfis
-                        </v-btn>
-                        <v-btn color="primary" class="ml-0 mt-3"
-                            @click="novoUsuario">
-                            Novo Usuário
-                        </v-btn>
-                </v-layout>
-            </v-flex>
-            <v-flex>
-                <v-layout column class="ma-3">
-                    <h1 class="headline">Resultado</h1>
-                    <v-divider />
-                    <template v-if="dados">
-                        <v-text-field label="ID" readonly
-                            v-model="dados.id" />
-                        <v-text-field label="Nome" readonly
-                            v-model="dados.nome" />
-                        <v-text-field label="Email" readonly
-                            v-model="dados.email" />
-                        <v-text-field label="Perfis" readonly
-                            :value="perfisRotulos" />
-                    </template>
-                </v-layout>
-            </v-flex>
-        </v-layout>
+      <v-layout>
+        <v-flex>
+          <v-layout column class="ma-3">
+            <h1 class="headline">Novo Usuário</h1>
+            <v-divider class="mb-3" />
+            <div v-if="erros">
+              <Erros :erros="erros" />
+            </div>
+            <v-text-field label="Nome" v-model="usuario.nome" />
+            <v-text-field label="E-mail" v-model="usuario.email" :rules="[v => !!v || 'E-mail é obrigatório', v => isEmailValid(v) || 'E-mail inválido']" />
+            <v-text-field label="Senha" v-model="usuario.senha" type="password" />
+            <v-select
+              label="Perfis"
+              v-model="usuario.perfis"
+              :items="perfis"
+              item-value="id"
+              item-text="rotulo"
+              attach
+              multiple
+              chips
+              deletable-chips
+            />
+            <v-btn class="ml-0 mt-3" @click="obterPerfis">Obter Perfis</v-btn>
+            <v-btn color="primary" class="ml-0 mt-3" @click="novoUsuario">Novo Usuário</v-btn>
+          </v-layout>
+        </v-flex>
+        <v-flex>
+          <v-layout column class="ma-3">
+            <h1 class="headline">Resultado</h1>
+            <v-divider />
+            <template v-if="dados">
+              <v-text-field label="ID" readonly v-model="dados.id" />
+              <v-text-field label="Nome" readonly v-model="dados.nome" />
+              <v-text-field label="Email" readonly v-model="dados.email" />
+              <v-text-field label="Perfis" readonly :value="perfisRotulos" />
+            </template>
+          </v-layout>
+        </v-flex>
+      </v-layout>
     </v-container>
-</template>
-
-<script>
-import Erros from '../comum/Erros'
-import gql from 'graphql-tag'
-
-export default {
+  </template>
+  
+  <script>
+  import Erros from '../comum/Erros';
+  import gql from 'graphql-tag';
+  import { required, email } from 'vuelidate/lib/validators';
+  
+  export default {
     components: { Erros },
     data() {
-        return {
-            usuario: {},
-            perfis: [
-                {
-                    id:1,
-                    rotulo: 'Administrador'
-                },{
-                    id:2,
-                    rotulo: 'Comum'
-                }
-            ],
-            dados: null,
-            erros: null
-        }
+      return {
+        usuario: {},
+        perfis: [
+          {
+            id: 1,
+            rotulo: 'Administrador',
+          },
+          {
+            id: 2,
+            rotulo: 'Comum',
+          },
+        ],
+        dados: null,
+        erros: null,
+      };
+    },
+    validations: {
+      usuario: {
+        nome: { required },
+        email: { required, email },
+        senha: { required },
+      },
     },
     computed: {
-        perfisRotulos() {
-            return this.dados && this.dados.perfis &&
-                this.dados.perfis.map(p => p.rotulo).join(', ')
-        },
-        perfisSelecionados() {
-            if(this.usuario.perfis) {
-                return this.usuario.perfis.map(id => ({ id }))
-            } else {
-                return null
-            }
+      perfisRotulos() {
+        return (
+          this.dados &&
+          this.dados.perfis &&
+          this.dados.perfis.map((p) => p.rotulo).join(', ')
+        );
+      },
+      perfisSelecionados() {
+        if (this.usuario.perfis) {
+          return this.usuario.perfis.map((id) => ({ id }));
+        } else {
+          return null;
         }
+      },
     },
     methods: {
-        novoUsuario() {
-            this.$api.mutate({
-                mutation: gql `
-                    mutation(
-                        $nome: String!
-                        $email: String!
-                        $senha: String!
-                        $perfis: [PerfilFiltro]
-                    ){
-                        novoUsuario(
-                            dados: {
-                                nome: $nome
-                                email: $email
-                                senha: $senha
-                                perfis: $perfis
-                            }
-                        ){
-                            id nome email perfis {rotulo}
-                        }
-                    }
-                `,
-                variables: {
-                    nome: this.usuario.nome,
-                    email: this.usuario.email,
-                    senha: this.usuario.senha,
-                    perfis: this.perfisSelecionados
-                }
-
-            }).then(resultado =>{
-                this.dados = resultado.data.novoUsuario
-                this.usuario = {}
-                this.erros = null
-            }).catch(e=> {
-                this.erros = e
-            })
-        },
-        obterPerfis() {
-        this.$api.query({
-            query: gql ` {perfis {id rotulo}} `
-        }).then(resultado => {
-            this.perfis = resultado.data.perfis
-            this.erros = null
-        }).catch(e=> {
-            this.erros = e
-        })
-            
+      novoUsuario() {
+        if (this.$v.$invalid) {
+          return;
         }
-    }
-}
-</script>
-
-<style>
-
-</style>
+  
+        this.$api
+          .mutate({
+            mutation: gql`
+              mutation(
+                $nome: String!
+                $email: String!
+                $senha: String!
+                $perfis: [PerfilFiltro]
+              ) {
+                novoUsuario(
+                  dados: {
+                    nome: $nome
+                    email: $email
+                    senha: $senha
+                    perfis: $perfis
+                  }
+                ) {
+                  id
+                  nome
+                  email
+                  perfis {
+                    rotulo
+                  }
+                }
+              }
+            `,
+            variables: {
+              nome: this.usuario.nome,
+              email: this.usuario.email,
+              senha: this.usuario.senha,
+              perfis: this.perfisSelecionados,
+            },
+          })
+          .then((resultado) => {
+            this.dados = resultado.data.novoUsuario;
+            this.usuario = {};
+            this.erros = null;
+          })
+          .catch((e) => {
+            this.erros = e;
+          });
+      },
+      obterPerfis() {
+        this.$api
+          .query({
+            query: gql` {perfis {id rotulo}} `,
+          })
+          .then((resultado) => {
+            this.perfis = resultado.data.perfis;
+            this.erros = null;
+          })
+          .catch((e) => {
+            this.erros = e;
+          });
+      },
+      isEmailValid(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
+  
